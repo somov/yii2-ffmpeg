6be8d6cb63d04cfa9b3c23c78d7dceb28b09f4f3
Nikolay Somov <somov.nn@gmail.com>
2022-03-13 21:24:47 +0200

    _Test _bootstrap.php fix

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -5,8 +5,8 @@ use yii\helpers\ArrayHelper;
 defined('YII_ENV') or define('YII_ENV', 'test');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
-require_once __DIR__ .  '/../../../app/vendor/yiisoft/yii2/Yii.php';
-require __DIR__ .'/../../../app/vendor/autoload.php';
+require_once __DIR__ .  '/../../vendor/yiisoft/yii2/Yii.php';
+require __DIR__ .'/../../vendor/autoload.php';
 
 $dir = dirname(__DIR__);
 
